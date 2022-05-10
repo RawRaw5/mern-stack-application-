11f79b26e972f1cc85c0484430a4a2f29e8a2029
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-05-09 21:21:26 -0700

    installed dependencies

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+// const serverRouter = require("./routes/routes")
+
+// app.use("/blog", serverRouter)
 
 app.get("/", (req, res) => {
     res.send("hello world");
