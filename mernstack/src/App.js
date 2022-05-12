e5ff45ba294004194eac054c4377459463b5dacb
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-05-11 21:03:27 -0700

    added router, making post route next

--- mernstack/src/App.js
+++ mernstack/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
 import CreatePost from './create_post';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 function App() {
   return (
-    <div className="App">
-     <CreatePost />
-     
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<CreatePost />}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- mernstack/src/create_post.js
+++ mernstack/src/create_post.js
@@ -7,17 +7,26 @@ import Button from 'react-bootstrap/Button'
 
 
 function CreatePost() {
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        alert('You have created a post')
+    }
+
+
   return (
       <div>
           <Card style={{ width: '20rem'}}>
             <Card.Header as="h5">Create Post</Card.Header>
             <Card.Body>
-                <Card.Text>
-                <input type='text'/>
-                </Card.Text>
-                <Button variant="primary">Post</Button>
+                <form onSubmit={handleSubmit}>
+                <input id='text-box' type='text' placeholder="What's on your mind?"/>
+                <Card.Text>-Anonymous</Card.Text>
+                </form>
+                <Button onClick={handleSubmit} variant="primary">Post</Button>
             </Card.Body>
             </Card>
+            
       </div>
   )
 }
