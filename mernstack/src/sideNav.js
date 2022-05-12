da60c10b247e476b3bb44e5b7a6dd983a8463f11
Jesus Flores <jesus4flores@gmail.com>
2022-05-11 21:37:11 -0700

    added code to navbar

---
+++ mernstack/src/sideNav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const SideNav = (props) => {
+    return(
+        <div className="sideNav">
+            <a href="#home">Home</a>
+            <a href="#basketball">Basketball</a>
+            <a href="#soccer">Soccer</a>
+            <a href="#baseball">Baseball</a>
+            <a href="#about">About</a>
+        </div>
+    );
+};
+export default SideNav;
