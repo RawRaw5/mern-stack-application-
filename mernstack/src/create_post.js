d6fcf842369a2fbf26d56a98df06286bb870e252
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-05-14 11:28:57 -0700

    create_post page returns the text-input as json

--- mernstack/src/create_post.js
+++ mernstack/src/create_post.js
@@ -8,9 +8,12 @@ import Button from 'react-bootstrap/Button'
 
 function CreatePost() {
 
+    
     const handleSubmit = event => {
         event.preventDefault()
-        alert('You have created a post')
+        var myInput = document.getElementById('text-box').value
+    console.log("test", myInput)
+        alert(JSON.stringify(myInput))
     }
 
 
