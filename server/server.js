9a0609e6ef0e729c844d8ac6c0f701a65a85192c
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-05-18 21:29:21 -0700

    initiated cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+import cors from "cors";
 
 // EXPRESS SETTINGS
 const app = express();
--- server/views/success.jsx
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-
-function success() {
-    return (
-        <h1>Success!</h1>
-    );
-};
-
-module.exports = success;
