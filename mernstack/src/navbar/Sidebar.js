44acc2d14166949c52d2640e9e850a2a2d2ebe9b
Jesus Flores <jesus4flores@gmail.com>
2022-05-18 20:13:29 -0700

    minor changes

--- mernstack/src/navbar/Sidebar.js
+++ mernstack/src/navbar/Sidebar.js
@@ -11,7 +11,7 @@ function Sidebar() {
                 <li 
                 key={key}
                 className="row"
-                id={window.location.pathname == val.link ? "active" : ""}
+                id={window.location.pathname === val.link ? "active" : ""}
                 onClick={()=> {
                     window.location.pathname = val.link;
                     }}
