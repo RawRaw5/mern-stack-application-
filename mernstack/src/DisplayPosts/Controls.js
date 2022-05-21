879a68130decc906e9fc3eac8ee3839ef3f1a30b
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-05-20 22:23:33 -0700

    clickable cards

--- mernstack/src/DisplayPosts/Controls.js
+++ mernstack/src/DisplayPosts/Controls.js
@@ -7,19 +7,22 @@ const Controls = () => {
 
     const deletePost = async (id) => {
         try {
-            const res = await axios.delete('https://mernstack-application.herokuapp.com/posts/:id');
+            const res = await axios.delete('https://mernstack-application.herokuapp.com/posts/,', {id});
             console.log("Item successfully deleted.");
         } catch (error) {
             alert(error);
+            console.log(error.message)
         }
     };
 
     return (
         <>
-            <Button className="me-3" variant="danger">
+            <Button className="me-3" variant="danger" onClick={deletePost}>
                 Delete
             </Button>
-            <Button>Edit</Button>
+            <Button>
+                Edit
+            </Button>
         </>
     );
 };
--- mernstack/src/DisplayPosts/DisplayPosts.js
+++ mernstack/src/DisplayPosts/DisplayPosts.js
@@ -6,6 +6,7 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 import Controls from "./Controls";
+import "./display.css";
 
 const DisplayPosts = () => {
     const [data, setData] = useState([]);
@@ -21,21 +22,53 @@ const DisplayPosts = () => {
             });
     }, []);
 
+    const handleDelete = async (id) => {
+        try {
+            const res = await axios.delete(
+                "https://mernstack-application.herokuapp.com/posts/,",
+                { id }
+            );
+            console.log("Item successfully deleted.");
+        } catch (error) {
+            alert(error);
+            console.log(error.message);
+        }
+    };
+
+    // GET request for remote image in node.js
+    axios({
+        method: "delete",
+        url: `https://mernstack-application.herokuapp.com/posts/`,
+        responseType: "stream",
+    }).then(function(response) {
+        // response.data.pipe(fs.createWriteStream("ada_lovelace.jpg"));
+    });
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log('cars was clicked')
+        console.log()
+    }
+
     return (
         <Container fluid className="p-3 my-3">
             <Row xs={1} md={1} className="g-4">
                 {data.map((da) => (
-                    <Col key={da._id}>
+                    <Col key={da._id} className="card-item " onClick={handleClick}>
                         <Card>
                             <Card.Body>
-                                <Card.Title className="mb-1">
+                                <Card.Title className="mb-2">
                                     {da.title}
                                 </Card.Title>
                                 <Card.Subtitle className="mb-3">
                                     {da.author}
                                 </Card.Subtitle>
                                 <Card.Text>{da.body}</Card.Text>
-                                <Controls />
+                                <Card.Text>{da._id}</Card.Text>
+                                <Button variant="danger" onClick={handleDelete}>
+                                    Delete
+                                </Button>
+                                {/* <Controls /> */}
                             </Card.Body>
                         </Card>
                     </Col>
