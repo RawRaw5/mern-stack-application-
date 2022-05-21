7792133ce840279aab088f244720041ca8f94880
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-05-21 10:06:56 -0700

    fixed delete button

--- mernstack/src/DisplayPosts/Controls.js
+++ mernstack/src/DisplayPosts/Controls.js
@@ -2,24 +2,42 @@ import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 
-const Controls = () => {
-    const [data, setData] = useState("");
+const Controls = (props) => {
+    const {pullFunction, postId} = props
+    // const [data, setData] = useState("");
 
     const deletePost = async (id) => {
         try {
-            const res = await axios.delete('https://mernstack-application.herokuapp.com/posts/', {id});
-            console.log("Item successfully deleted.");
+            console.log(id)
+            const res = await axios.delete('https://mernstack-application.herokuapp.com/posts/' + id);
+            pullFunction();
+            console.log('successfully deleted')
         } catch (error) {
-            alert(error);
+            alert('delete post' + error);
         }
     };
 
+    const editPost = async (id) => {
+        try {
+            const res = await axios.put('https://mernstack-application.herokuapp.com/posts/', {id}, {
+                title: "",
+                body: "",
+                author: "",
+                sport: ""
+            })
+            console.log("Item successfully updated")
+        } catch (error) {
+            // alert(error)
+        }
+    }
+
+
     return (
         <>
-            <Button className="me-3" variant="danger">
+            {/* <Button onClick={()=>deletePost(postId)} className="me-3" variant="danger">
                 Delete
-            </Button>
-            <Button>Edit</Button>
+            </Button> */}
+            <Button>Like</Button>
         </>
     );
 };
